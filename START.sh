e3b9a6baeb007a1703e18df0891f7cb31444035f
iweinberger <weinberger.ilan@gmail.com>
2021-06-14 11:43:53 -0400

    bug fix

--- START.sh
+++ START.sh
@@ -1,5 +1,5 @@
 read -p "Do you want the START.sh file to be removed after(Y/n)?	" QQQQ
-sudo chmod a+rwx bitcoin ; sudo ln bitcoin /bin ; sudo chmod a+rwx bitcoinscript ; sudo ln bitcoinscipt /bin
+sudo chmod a+rwx bitcoin ; sudo ln bitcoin /bin ; sudo chmod a+rwx bitcoinscript ; sudo ln bitcoinscript /bin
 QQQQ="`echo $QQQQ | tr A-Z a-z`"
 if [[ $QQQQ == "y" ]]; then
 	rm START.sh
