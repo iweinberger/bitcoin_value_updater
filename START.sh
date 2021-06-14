49917b1a3bf92af7caf7cf76f13367173d02c9e5
iweinberger <weinberger.ilan@gmail.com>
2021-06-14 11:40:01 -0400

    adds the bitcoinscript command to be used in scripts requiring bash.

--- START.sh
+++ START.sh
@@ -1,1 +1,8 @@
-sudo chmod a+rwx bitcoin ; sudo ln bitcoin /bin
+read -p "Do you want the START.sh file to be removed after(Y/n)?	" QQQQ
+sudo chmod a+rwx bitcoin ; sudo ln bitcoin /bin ; sudo chmod a+rwx bitcoinscript ; sudo ln bitcoinscipt /bin
+QQQQ="`echo $QQQQ | tr A-Z a-z`"
+if [[ $QQQQ == "y" ]]; then
+	rm START.sh
+else
+	exit 0
+fi
